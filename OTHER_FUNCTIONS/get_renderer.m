4dcd5cafd5a44269971c5b48130fcab79577b26e
Beliy Nikita <beliy.nikita@uliege.be>
2022-11-21 16:48:55 +0100

    Fixed parameters issues for get_renderer function

--- OTHER_FUNCTIONS/get_renderer.m
+++
@@ -1,7 +0,0 @@
-function params = get_renderer(xSPM)
-
-  params.XYZ = xSPM.XYZ;
-  params.t = xSPM.Z;
-  params.mat = xSPM.M;
-  params.dim = xSPM.DIM;
-end
--- private/E_STAT_script.m
+++ private/E_STAT_script.m
@@ -1,7 +1,6 @@
 function mat_file = E_STAT_script(files, render, mask, controls)
   %declaring useful variables
   global JOB_DIR
-  global xSPM
 
   files = cellstr(files);
   controls = cellstr(controls);
@@ -31,12 +30,12 @@ function mat_file = E_STAT_script(files, render, mask, controls)
   export_fig 'Hypo.jpg';
   
   %spm_render_DOC(struct('XYZ', xSPM.XYZ, 't',xSPM.Z,'mat', xSPM.M,'dim', xSPM.DIM),1,render,1)
-  spm_render_DOC(get_renderer(xSPM),1,render,1)
+  spm_render_DOC(get_renderer(),1,render,1);
   %saveas(gcf, 'Render_hypo_new.jpg');
   export_fig 'Render_hypo_new.jpg';
 
   %spm_render_DOC(struct('XYZ', xSPM.XYZ, 't',xSPM.Z,'mat', xSPM.M,'dim', xSPM.DIM),nan,render)
-  spm_render_DOC(get_renderer(xSPM),nan,render)
+  spm_render_DOC(get_renderer(),nan,render);
   spm_figure('colormap','gray-cool');
   %saveas(gcf, 'Render_hypo_old.jpg');
   export_fig 'Render_hypo_old.jpg';
